cc3c140a18ec9d85f0372883e3e8c55f266c08fc
Faaris Sayyid Al-Ghozali <116880373+faaris23@users.noreply.github.com>
2024-03-05 23:48:45 +0700

    Update toko.java

--- src/com/tugas/toko.java
+++ src/com/tugas/toko.java
@@ -28,10 +28,9 @@ public class toko {
     }
 
 	void display(){
+        System.out.println("====================================");
 		System.out.println("\nNama \t\t: " + this.namaToko);
         System.out.println("Penjelasan : \t\t" + this.penjelasan);
-        System.out.println("====================================");
-        System.out.println("SEMBAKO GOLDEN EXPERINCE");
         System.out.println("Keunggulan  \t\t: " + this.keunggulan);
         System.out.println("\t\tTOP 3 PENDAPATAN UTAMA: \t\t");
         System.out.println("1. \t: " + this.mainIncome1);
@@ -41,6 +40,7 @@ public class toko {
 	}
     
     public void mototoko(){
+        //Encapsulation (ini sebenarnya kek orang teriak gitu, tapi bingung nulis gimana, soalny kalo TerikkanMoto() malah aneh)
         System.out.println("MOTTO TOKO KAMI ADALAH : " + this.mototoko);
     }
 }
